3ee830001ce59b63f5349f18c539259bac028a61
LoliDens <104903662+LoliDens@users.noreply.github.com>
2022-10-14 17:52:40 +0500

    Добавьте файлы проекта.

---
+++ homeWork_string/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homeWork_string
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            int age;
+            string bloodType ;
+
+            Console.Write("Введите свое имя: ");
+            name = Console.ReadLine();
+            Console.Write("Введите свой возраст: ");
+            age = int.Parse(Console.ReadLine());
+            Console.Write("Введите стою группу крови: ");
+            bloodType = Console.ReadLine();
+
+            Console.Write($"Вас завут {name} ,вам {age} лет и у вас {bloodType} группа крови");
+            Console.ReadKey();
+            
+
+        }
+    }
+}
